e0a51eb5d916abd6e801fc641ff08baebcd96bef
Yang Guo <gy@fir.im>
2018-09-03 13:45:04 +0800

    init agent queue

--- agent/src/main/java/com/flowci/agent/config/QueueConfig.java
+++ agent/src/main/java/com/flowci/agent/config/QueueConfig.java
@@ -16,10 +16,18 @@
 
 package com.flowci.agent.config;
 
+import com.flowci.agent.manager.CmdManager;
 import com.flowci.domain.Jsonable;
+import com.flowci.domain.Settings;
+import com.flowci.domain.Settings.RabbitMQ;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -34,11 +42,52 @@ public class QueueConfig {
     private final Jackson2JsonMessageConverter queueMessageConverter =
         new Jackson2JsonMessageConverter(Jsonable.getMapper());
 
-//    @Bean("queueTemplate")
-//    public RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
-//        RabbitTemplate template = new RabbitTemplate(factory);
-//        template.setMessageConverter(queueMessageConverter);
-//        return template;
-//    }
+    @Autowired
+    private Settings agentSettings;
 
+    @Bean("agentQueue")
+    public Queue jobQueue() {
+        String agentQueue = agentSettings.getAgent().getQueueName();
+        return new Queue(agentQueue, true);
+    }
+
+    @Bean("callbackQueue")
+    public Queue callbackQueue() {
+        String queueName = agentSettings.getCallbackQueueName();
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public ConnectionFactory factory() {
+        RabbitMQ mq = agentSettings.getQueue();
+        CachingConnectionFactory factory = new CachingConnectionFactory(mq.getHost(), mq.getPort());
+        factory.setUsername(mq.getUsername());
+        factory.setPassword(mq.getPassword());
+        return factory;
+    }
+
+    @Bean
+    public MessageListenerAdapter adapter(CmdManager cmdManager) {
+        MessageListenerAdapter adapter = new MessageListenerAdapter(cmdManager, "onCmdReceived");
+        adapter.setMessageConverter(queueMessageConverter);
+        return adapter;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer container(ConnectionFactory factory, MessageListenerAdapter adapter) {
+        String queueName = agentSettings.getAgent().getQueueName();
+
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(factory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(adapter);
+        return container;
+    }
+
+    @Bean("queueTemplate")
+    public RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
+        RabbitTemplate template = new RabbitTemplate(factory);
+        template.setMessageConverter(queueMessageConverter);
+        return template;
+    }
 }
