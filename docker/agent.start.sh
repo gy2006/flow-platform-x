66dc49f9603d7308c6ed7258c328ed4703b5bc74
Yang Guo <gy@fir.im>
2018-09-17 12:44:05 +0800

    update docker compose to add -d

--- docker/agent.start.sh
+++ docker/agent.start.sh
@@ -3,4 +3,4 @@
 export AGENT_TOKEN=458b7484-e6ce-4af4-a582-ddec163fbaa9
 export AGENT_SERVER_URL=http://127.0.0.1:8080
 
-docker-compose -f docker-compose.agent.yml up
+docker-compose -f docker-compose.agent.yml up -d
--- docker/core.start.sh
+++ docker/core.start.sh
@@ -2,4 +2,4 @@
 
 export HOST_IP=127.0.0.1
 
-docker-compose -f docker-compose.core.yml up
+docker-compose -f docker-compose.core.yml up -d
