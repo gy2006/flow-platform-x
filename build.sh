14abbacceceacb5fd5efb72414efb7eea1df0831
gy2006 <benqyang.2006.se@gmail.com>
2019-09-02 15:47:26 +0200

    add version tag to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+version=$1
+
+if [[ -n ${version} ]]; then
+  VersionTag="-t flowci/core:$version"
+fi
+
 mvn clean package -Dmaven.test.skip=true
 
-docker build -f ./core/Dockerfile -t flowci/core:latest ./core
+docker build -f ./core/Dockerfile -t flowci/core:latest $VersionTag ./core
